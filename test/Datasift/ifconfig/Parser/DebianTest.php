9f2c72a074074b6d2b586c2844a98f26f7bc4df9
muteor <muteor@deviantart.com>
2014-03-31 10:22:07 +0100

    Add debian parser

---
+++ test/Datasift/ifconfig/Parser/DebianTest.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Copyright (c) 2013-present Mediasift Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Libraries
+ * @package   IfconfigParser\Test
+ * @author    Michael Heap <michael.heap@datasift.com>
+ * @copyright 2013-present Mediasift Ltd www.datasift.com
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link      http://github.com/datasift/ifconfig-parser-php
+ */
+
+use Datasift\IfconfigParser\Parser\Debian;
+
+require_once __DIR__.'/../ParserTestBase.php';
+class DebianTest extends ParserTestBase {
+
+    public function setUp(){
+        $this->parser = new Ubuntu;
+    }
+
+    public function exampleIfconfigProvider(){
+
+        return array(
+            array(
+				'lo        Link encap:Local Loopback  
+          inet addr:127.0.0.1  Mask:255.0.0.0
+          inet6 addr: ::1/128 Scope:Host
+          UP LOOPBACK RUNNING  MTU:16436  Metric:1
+          RX packets:874 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:874 errors:0 dropped:0 overruns:0 carrier:0
+          collisions:0 txqueuelen:0 
+          RX bytes:120735 (120.7 KB)  TX bytes:120735 (120.7 KB)
+
+wlan0     Link encap:Ethernet  HWaddr 84:3a:4b:ca:15:ac  
+          inet addr:192.168.102.221  Bcast:192.168.102.255  Mask:255.255.255.0
+          inet6 addr: fe80::863a:4bff:feca:15ac/64 Scope:Link
+          UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
+          RX packets:4447 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:4518 errors:0 dropped:0 overruns:0 carrier:0
+          collisions:0 txqueuelen:1000 
+		  RX bytes:2697453 (2.6 MB)  TX bytes:1142287 (1.1 MB)',
+          array(
+              $this->getNetworkInterface("lo", "127.0.0.1"),
+              $this->getNetworkInterface("wlan0", "192.168.102.221")
+          )
+      )
+  );
+    }
+
+}
+
